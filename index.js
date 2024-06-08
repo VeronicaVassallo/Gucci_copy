94780034d1782b0464118aeea185f5532ecf7ce9
VeronicaVassallo <vvassallo94@gmail.com>
2024-06-08 16:25:58 +0200

    Fix animation logo

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const nav = document.getElementById("nav");
 const plusContact = document.getElementById("contact");
+const logo = document.getElementById("logo");
 
 //Function onScroll add e remove class
 const toggleClassNav = () => {
 	if (window.scrollY > 0) {
 		nav.classList.add("bg-white", "text-dark");
 		plusContact.classList.add("visible");
+		logo.classList.remove("logoAnimationReverse");
+		logo.classList.add("logoAnimation");
 		nav.classList.remove("text-light");
 		plusContact.classList.remove("invisible");
 	} else {
@@ -13,6 +16,8 @@ const toggleClassNav = () => {
 		plusContact.classList.remove("visible");
 		nav.classList.add("text-light");
 		plusContact.classList.add("invisible");
+		logo.classList.remove("logoAnimation");
+		logo.classList.add("logoAnimationReverse");
 	}
 };
 
