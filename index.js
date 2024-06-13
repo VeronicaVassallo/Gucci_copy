592ffe5f9d39105c598c583439cd66abb8cc23bb
VeronicaVassallo <vvassallo94@gmail.com>
2024-06-13 23:06:34 +0200

    search box css

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const logo = document.getElementById("logo");
 const containerMenuOpened = document.getElementById("containerMenuOpened");
 const menuOpened = document.getElementById("menuOpened");
 const body = document.getElementsByTagName("body")[0];
+const searchBox = document.getElementById("searchMenu");
 
 //Function onScroll add e remove class
 const toggleClassNav = () => {
@@ -31,4 +32,10 @@ const openMenu = () => {
 	containerMenuOpened.classList.toggle("showMenuOpened");
 	menuOpened.classList.toggle("d-none");
 	body.classList.toggle("hiddenOverflow");
+};
+
+//Function show searchBar
+const openSearchBox = () => {
+	debugger;
+	searchBox.classList.toggle("d-none");
 };
